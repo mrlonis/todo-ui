6c309687d2ab1e883251833d253e370eed6bd255
Matthew Lonis <mattlonis@gmail.com>
2024-07-19 20:58:27 -0400

    Fixing eslint issues

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,14 @@
+import { CdkAccordionModule } from '@angular/cdk/accordion';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CdkAccordionModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  items = ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5'];
+  expandedIndex = 0;
+}
