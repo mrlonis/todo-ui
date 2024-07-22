f89c197425107246023dc434f9a09749a9fce8a0
Matthew Lonis <mattlonis@gmail.com>
2024-07-21 20:01:46 -0400

    Swapping off CDK Accordian

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,8 +1,9 @@
 import { CdkAccordionModule } from '@angular/cdk/accordion';
 import { Dialog, DialogModule } from '@angular/cdk/dialog';
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import { MatExpansionModule } from '@angular/material/expansion';
 import { TodoItem } from '../../interfaces';
 import { ApiService } from '../../services';
 import { CreateItemDialogComponent } from '../create-item-dialog/create-item-dialog.component';
@@ -11,11 +12,14 @@ import { TodoItemComponent } from '../todo-item/todo-item.component';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CdkAccordionModule, DialogModule, FormsModule, MatButtonModule, TodoItemComponent],
+  imports: [CdkAccordionModule, DialogModule, FormsModule, MatButtonModule, MatExpansionModule, TodoItemComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HomeComponent implements OnInit {
+  readonly panelOpenState = signal(false);
+
   items: TodoItem[] = [];
   expandedIndex = 0;
   animal?: string;
