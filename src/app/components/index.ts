dd95c27559372e55dcbc5e9d7a7e7b0a7617bd90
Matthew Lonis <mattlonis@gmail.com>
2024-07-28 23:14:32 -0400

    Adding stuff

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -2,5 +2,6 @@ export * from './add-new-pi-dialog';
 export * from './add-new-sprint-dialog';
 export * from './create-item-dialog';
 export * from './home';
+export * from './navigation';
 export * from './page-not-found';
 export * from './todo-item';
---
+++ src/app/components/navigation/navigation.component.ts
@@ -0,0 +1,56 @@
+import { MediaMatcher } from '@angular/cdk/layout';
+import { ChangeDetectorRef, Component, OnDestroy } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterModule, RouterOutlet } from '@angular/router';
+
+export interface NavItem {
+  link: string;
+  display: string;
+}
+
+@Component({
+  selector: 'app-navigation',
+  standalone: true,
+  imports: [
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
+    RouterOutlet,
+    RouterModule,
+  ],
+  templateUrl: './navigation.component.html',
+  styleUrl: './navigation.component.scss',
+})
+export class NavigationComponent implements OnDestroy {
+  mobileQuery: MediaQueryList;
+
+  navLinks: NavItem[] = [
+    { link: '/', display: 'Home' },
+    { link: '/archive', display: 'Archive' },
+  ];
+
+  private _mobileQueryListener: () => void;
+
+  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    try {
+      this.mobileQuery.addEventListener('change', () => {
+        this._mobileQueryListener();
+      });
+    } catch (e) {
+      console.error(e);
+      this.mobileQuery.addListener(this._mobileQueryListener);
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
+  }
+}
